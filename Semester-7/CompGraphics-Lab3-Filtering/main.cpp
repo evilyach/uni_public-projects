bb03bbfcff695884aa64eeafc6f410e429b3ade5
Ilya Chesalin <evilyach@protonmail.com>
2019-10-20 22:24:04 +0300

    Semester-7/CompGraphics-Lab3-Filtering: Small fixes

--- Semester-7/CompGraphics-Lab3-Filtering/main.cpp
+++ Semester-7/CompGraphics-Lab3-Filtering/main.cpp
@@ -1,11 +1,24 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QSplashScreen>
+#include <QTimer>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    const char *splashcreen_filename = "splash.png";
+    QPixmap pixmap(splashcreen_filename);
+
+    QSplashScreen splash(pixmap, Qt::WindowStaysOnTopHint);
+    splash.show();
+
     MainWindow w;
-    w.show();
+
+    int duration = 2000;
+    QTimer::singleShot(duration, &splash, SLOT(close()));
+    QTimer::singleShot(duration, &w, SLOT(show()));
+
     return a.exec();
 }
