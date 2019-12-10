074353f31cdb524e67943d19d95261377921798f
Ilya Chesalin <evilyach@protonmail.com>
2019-12-10 22:42:36 +0000

    [7/Crypto] Added encrypt functionality

--- Semester-7/Crypto-Lab4-DigitalSignature/index.js
+++ Semester-7/Crypto-Lab4-DigitalSignature/index.js
@@ -1,10 +1,39 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const nodeRSA = require('node-rsa');
+const fs = require('fs');
+const app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/html/doc_sender.html'));
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.get('/check', (req, res) => {
+    res.sendFile(path.join(__dirname + '/html/doc_reciever.html'));
+});
+
+app.post('/getPublicKey', (req, res) => {
+    const plainText = req.body.plainText;
+
+    const key = new nodeRSA({b: 2048});
+
+    const encrypted = key.encrypt(plainText, 'base64');
+    console.log(encrypted);
+
+    res.send(encrypted);
+});
+
+app.post('checkPublicKey', (req, res) => {
+
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}!`);
 });
