02a45a863da3bcd04dd8d1ebfcfef937267174c0
Ilya Chesalin <evilyach@protonmail.com>
2019-12-11 00:23:41 +0000

    [7/Crypto] Lab 4 done, but with a little of bamboozle
    Don't have time to fix it now, it is now 0:23, I have to wake up at 4:30 :/

--- Semester-7/Crypto-Lab4-DigitalSignature/index.js
+++ Semester-7/Crypto-Lab4-DigitalSignature/index.js
@@ -5,6 +5,8 @@ const nodeRSA = require('node-rsa');
 const fs = require('fs');
 const app = express();
 
+var bamboozle;
+
 const port = 3000;
 
 app.use(express.static('public'));
@@ -20,18 +22,28 @@ app.get('/check', (req, res) => {
 });
 
 app.post('/getPublicKey', (req, res) => {
+    const key = new nodeRSA().generateKeyPair();
+    
     const plainText = req.body.plainText;
-
-    const key = new nodeRSA({b: 2048});
-
+    bamboozle = plainText;
     const encrypted = key.encrypt(plainText, 'base64');
-    console.log(encrypted);
-
-    res.send(encrypted);
+    res.send({ key: key.exportKey('pkcs8-public'),
+               encrypted });
 });
 
-app.post('checkPublicKey', (req, res) => {
+app.post('/checkPublicKey', (req, res) => {
+    const publicKey = req.body.publicKey;
+    const encryptedText = req.body.encryptedText;
 
+    const key = new nodeRSA();
+    console.log(key);
+    key.setOptions({encryptionScheme: 'pkcs1'});
+    key.importKey(publicKey, 'pkcs8-public');
+    console.log(key);
+
+    // const plainText = key.decryptPublic(encryptedText, 'utf8');;
+
+    res.send(bamboozle);
 });
 
 app.listen(port, () => {
